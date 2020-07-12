df007a65d24f8b26d0d10eac428ed8df6da3ad96
edogawa_kazuki <galad.zhao@gmail.com>
2020-07-13 01:24:52 +0800

    - Show event list - Sort by field - Show Single event - Pagination

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require ('cors');
+var session = require('express-session')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
 
 var app = express();
@@ -14,14 +14,29 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// allow cors
+app.use(cors());
+
+
+// logger
 app.use(logger('dev'));
+
+// express parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// cookie
 app.use(cookieParser());
+
+// session
+app.use(session({
+  secret: 'csci2720'
+}))
+
+// static file
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// router
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+const { sortBy } = require('lodash');
 
-router.get('/', (req, res, next) => {
-    res.send("API!");
+// connect to DB
+mongoose.connect('mongodb://localhost:27017/csci2720');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection errr:'));
+db.once('open', () => {
+  console.log("connection is open");
 })
+
+// Event schema
+var EventSchema = mongoose.Schema({
+    eventId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    loc: { type: String },
+    quota: { type: Number }
+});
+
+var Event = mongoose.model('event', EventSchema);
+
+router.get('/events', (req, res, next) => {
+    res.redirect('./events/1');
+});
+
+router.get('/events/page/:page/sortBy/:sortBy', (req, res, next) => {
+    console.log(req.params.page, req.params.sortBy)
+    Event.find(
+        {},
+        'event_summary event_location event_date event_org',
+        function (err, e) {
+            if(err)
+                res.send(err);
+            if(e){
+                res.send(e);
+            }else{
+                res.send({err: "No event found"});
+            }
+        }
+    ).skip((req.params.page - 1) * 10).limit(10).sort(req.params.sortBy);
+})
+
+
+router.get('/search/:keyword', (req, res, next) => {
+    Event.find(
+        {name: {'$regex' : req.params.keyword}},
+        'event_summary event_location event_date event_org',
+        function (err, e) {
+            if(err)
+            res.send(err);
+            console.log(e);
+            if(e){
+                res.send(e);
+            }else{
+                res.send({err: "No event found"});
+            }
+        }
+    );
+})
+
 
 module.exports = router
